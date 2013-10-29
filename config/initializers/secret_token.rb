db6d45cbf01dfa0723fec90336059c0a8a422ae5
8legged <jason.niemi@live.com>
2013-10-28 20:29:23 -0700

    initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pettracker::Application.config.secret_key_base = '877d0b0df2074b3e58158232a71041c53d623da3073c6b1b5daf14a37d4519d948e330a8b94f998d9b4dd9c5885b763f81fa7c1313dde98d9cbf31be69c87107'
